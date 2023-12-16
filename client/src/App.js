120c60c3b67cb338140d5fd4331ac13b6554c981
Hristo Ivanov <gw.hivanov@gmail.com>
2023-12-16 16:39:18 +0200

    css issues with footer when sidescroll is active fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { Header } from './Components/Header/Header';
-import { Footer } from './Components/Footer/Footer';
-import { Home } from './Components/Home/Home';
-import { Login } from './Components/Login/Login';
-import { Register } from './Components/Register/Register';
-import { Logout } from './Components/Logout/Logout';
-import { Forum } from './Components/Forum/Forum';
-import { ThreadPage } from './Components/Forum/ThreadPage/ThreadPage';
-import { Settings } from './Components/Settings/Settings';
+import { Header } from "./Components/Header/Header";
+import { Footer } from "./Components/Footer/Footer";
+import { Home } from "./Components/Home/Home";
+import { Login } from "./Components/Login/Login";
+import { Register } from "./Components/Register/Register";
+import { Logout } from "./Components/Logout/Logout";
+import { Forum } from "./Components/Forum/Forum";
+import { ThreadPage } from "./Components/Forum/ThreadPage/ThreadPage";
+import { Settings } from "./Components/Settings/Settings";
 
-import { AuthProvider } from './contexts/AuthContext';
-import { ForumProvider } from './contexts/ForumContext';
-import { UserProvider } from './contexts/UserContext';
+import { AuthProvider } from "./contexts/AuthContext";
+import { ForumProvider } from "./contexts/ForumContext";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
     return (
@@ -25,14 +25,22 @@ function App() {
 
                         <main id="main-content">
                             <Routes>
-                                <Route path='/' element={<Home />} />
-                                <Route path='/login' element={<Login />} />
-                                <Route path='/register' element={<Register />} />
-                                <Route path='/logout' element={<Logout />} />
-                                <Route path='/forum' element={<Forum />} />
-                                <Route path='/forum/:postId' element={<ThreadPage />} />
-                                <Route path='/settings' element={<Settings />} />
-
+                                <Route path="/" element={<Home />} />
+                                <Route path="/login" element={<Login />} />
+                                <Route
+                                    path="/register"
+                                    element={<Register />}
+                                />
+                                <Route path="/logout" element={<Logout />} />
+                                <Route path="/forum" element={<Forum />} />
+                                <Route
+                                    path="/forum/:postId"
+                                    element={<ThreadPage />}
+                                />
+                                <Route
+                                    path="/settings"
+                                    element={<Settings />}
+                                />
 
                                 {/*
                     <Route element={<RouteGuard />}>
