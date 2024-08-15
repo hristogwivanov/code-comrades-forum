966df65509fd613f094e2d67eb1a14a3472acd1d
Hristo Ivanov <gw.hivanov@gmail.com>
2024-08-15 14:28:37 +0300

    hover effect fixed on the threadlist items

--- client/src/Components/Forum/ThreadList/ThreadListItem/ThreadListItem.js
+++ client/src/Components/Forum/ThreadList/ThreadListItem/ThreadListItem.js
@@ -3,7 +3,6 @@ import styles from "./ThreadListItem.module.css";
 
 export const ThreadListItem = ({ _id, postTitle, userName }) => {
     return (
-        <>
             <td className={styles["Item"]}>
                 <Link to={`/thread/${_id}`} className={styles["Link"]}>
                     <strong>{postTitle}</strong>
@@ -11,6 +10,5 @@ export const ThreadListItem = ({ _id, postTitle, userName }) => {
                     published by <strong>{userName}</strong>
                 </Link>
             </td>
-        </>
     );
 };
